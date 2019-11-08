28d613749168ede8d2e4b5745aaa8fbfc2107fcc
onceltl <onceltl@gmail.com>
2019-11-08 02:15:27 -0800

    Add CustomTopo.

---
+++ CustomTopo.py
@@ -0,0 +1,51 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import irange, dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+
+class StarTopo(Topo):
+    def build(self, n=2,bw=10,delay=5,loss=0): #bw=10, delay='5ms',max_queue_size=1000, loss=10, use_htb=True
+        self.hostNum=n; 
+        self.switchNum = 1;
+        switch = self.addSwitch( 's%s' % 1 )
+        for i in irange( 1, n ):
+            host = self.addHost( 'h%s' %i ) 
+            self.addLink(host, switch,cls=TCLink,bw=bw,delay=delay,loss=loss)
+
+class CircleTopo(Topo):
+    def build(self, n=2,m=2,bw=10,delay=5,loss=0):
+        self.hostNum=n*m; 
+        self.switchNum = n;
+        switch ={};
+        for i in irange( 1, n ):
+            switch[i]= self.addSwitch( 's%s' % i )
+            for j in irange( 1, m ):
+                host = self.addHost( 's%s-h%s' %(i,j) ) 
+                self.addLink(host, switch[i],cls=TCLink,bw=bw,delay=delay,loss=loss)
+        for i in irange( 1, n-1 ):
+            self.addLink(switch[i],switch[i+1])
+        #self.addLink(switch[n],switch[1]);
+
+class NetTopo(Topo):
+    def build(self, n=2,m=2,bw=10,delay=5,loss=0):
+        self.hostNum=n * n *n; 
+        self.switchNum = n*n;
+        switch ={};
+        for i in irange( 1, n ):
+            for k in irange( 1, n ):
+                switch[(i-1)*n+k]= self.addSwitch( 's%s' % ((i-1)*n+k) )
+                for j in irange( 1, m ):
+                    host = self.addHost( 's%s-h%s' %((i-1)*n+k,j) ) 
+                    print 's%s-h%s' %((i-1)*n+k,j)
+                    self.addLink(host, switch[(i-1)*n+k],cls=TCLink,bw=bw,delay=delay,loss=loss)
+        
+        for i in irange( 1, n-1 ):
+            
+            for k in irange( 1, n-1 ):
+                self.addLink(switch[(i-1)*n+k],switch[i*n+k]);
+                self.addLink(switch[(i-1)*n+k],switch[(i-1)*n+k+1]);
+
+            self.addLink(switch[(i-1)*n+n],switch[i*n+n]);
+
--- setP2PNet.py
+++ setP2PNet.py
@@ -3,29 +3,34 @@
 
 from mininet.net import Mininet
 from mininet.topolib import TreeTopo
-
+from CustomTopo import *
 P2PNet=None
-
-def setupP2PNet(netType="net",weight=1,high=1):
+def setupP2PNet(netType="net",weight=1,high=1,bw=10,delay=5,loss=0):
 
     if netType == "circle":
         print("circle");
-
+        circletopo = CircleTopo(weight,high,bw=bw,delay=delay,loss=loss)
+        P2PNet = Mininet(topo=circletopo)
     elif netType == "tree":
         print("tree")
-        treetopo = TreeTopo(depth=weight,fanout=high)
+        treetopo = TreeTopo(depth=weight,fanout=high,bw=bw,delay=delay,loss=loss)
         P2PNet = Mininet(topo=treetopo)
-        
     elif netType == "star":
         print("star");
-    
+        startopo = StarTopo(weight * high)
+        P2PNet = Mininet(topo=startopo)
     elif netType == "net":
         print("net");
+        nettopo = NetTopo(weight,high,bw=bw,delay=delay,loss=loss)
+        P2PNet = Mininet(topo=nettopo)
+    else:
+        print("netType error!");
+        sys.exit(0);
 
     P2PNet.start();
-    P2PNet.pingAll() 
+    #P2PNet.pingAll() 
     P2PNet.stop()
 
 
 if __name__ == '__main__':
-    setupP2PNet(netType='tree',weight=2,high=2)
+    setupP2PNet(netType='net',weight=2,high=3)
