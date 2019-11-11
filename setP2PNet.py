a2d350f0226a5b2b1dbb426b9915e212441571d8
onceltl <onceltl@gmail.com>
2019-11-11 05:16:38 -0800

    Add control network cmd.

--- setP2PNet.py
+++ setP2PNet.py
@@ -3,12 +3,15 @@
 
 import time
 import sys
+import os
 from mininet.net import Mininet
 from mininet.topolib import TreeTopo
+from mininet.link import Link
 from CustomTopo import *
+from cmd import Cmd
 
 P2PNet=None
-
+defaultPort = 50000
 def xtermCMD(peer,ip1, port1, ip2, port2, mode):
     name="Host_%d: " % peer
 
@@ -23,7 +26,7 @@ def xtermCMD(peer,ip1, port1, ip2, port2, mode):
     return cmd % (name, file, args)
 
 def setupP2PNet(netType="net",weight=1,high=1,bw=10,delay=5,loss=0,mode='test'):
-
+    global P2PNet
     if netType == "circle":
         print("circle");
         circletopo = CircleTopo(weight,high,bw=bw,delay=delay,loss=loss)
@@ -47,17 +50,82 @@ def setupP2PNet(netType="net",weight=1,high=1,bw=10,delay=5,loss=0,mode='test'):
     P2PNet.start();
     for i, host in enumerate(P2PNet.hosts):
         print(i,host.IP)
-        cmd= xtermCMD(i,host.IP(), 50000, P2PNet.hosts[0].IP(), 50000, mode)
+        cmd= xtermCMD(i,host.IP(), defaultPort, P2PNet.hosts[0].IP(), defaultPort, mode)
         print(cmd)
         host.cmd(cmd)
         time.sleep(1)
     P2PNet.pingAll() 
-    #P2PNet.stop()
-    try:
-        while True:
-            time.sleep(60*60*24) # one day in seconds
-    except KeyboardInterrupt:
-        sys.exit(0)
 
+
+class NetworkCMD(Cmd):
+    intro = "Control simulation network. Type ? to list commands.\n"
+    prompt = "Bitcoin>>> "
+
+    def __init(self,bw=10,delay=5,loss=0,mode='test'):
+        reload(sys)
+        super(NetworkCMD, self).__init__()
+        sys.setdefaultencoding('utf-8')
+
+    def do_exit(self,arg):
+        print ('Bye!')
+        return True 
+    def do_KillXterm(self,line):
+        os.system("killall -SIGKILL xterm")
+        os.system("mn --clean > /dev/null 2>&1")
+
+    def do_ShowHosts(self, line):
+        for i, host in enumerate(P2PNet.hosts):
+            print(i, host.IP, host.name)
+
+    def do_AddHost(self, line):
+        
+        global P2PNet
+        id = len(P2PNet.hosts) + 1
+
+        newHost = P2PNet.addHost("h%d" % id)
+
+        switch = P2PNet.switches[0];
+
+        P2PNet.addLink(switch, newHost);
+
+        slink = Link(switch, newHost)
+        switch.attach(slink);
+        switch.start(P2PNet.controllers);
+
+        newHost.configDefault(defaultRoute=newHost.defaultIntf())
+
+        print(P2PNet.hosts[0].cmd("ping -c1 %s" % newHost.IP() )) 
+
+        print(newHost.cmd("ping -c1 10.0.0.1"))
+
+        cmd = xtermCMD(id,newHost.IP(),defaultPort,P2PNet.hosts[0].IP(),defaultPort, 'test')
+
+        print(cmd)
+
+        newHost.cmd(cmd)
+
+        print("Started new node: %s" % newHost)
+
+    def do_DelHost(self, line):
+
+        global P2PNet
+        args = line.split()
+        if (len(args) != 1):
+            print("Expected 1 argument, %d given" % len(args))
+        else:
+            hostName = args[0].strip()
+            node = None;
+            for host in P2PNet.hosts:
+                if host.name == hostName:
+                    node = host;
+                    break;
+            if node == None:
+                print("Wrong hostName.")
+            else:
+                P2PNet.delNode(node)
+                print("Deleted %s" %hostName)
+                
 if __name__ == '__main__':
-    setupP2PNet(netType='star',weight=2,high=3)
+    setupP2PNet(netType='star',weight=3,high=2)
+    myCmd = NetworkCMD()
+    myCmd.cmdloop()
